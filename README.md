bdf9e972e82a9b162514afa2693da7e90833d00f
79124 <141174094+79124@users.noreply.github.com>
2024-03-13 12:47:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo
+# demo-git
+# demo-git
---
+++ index.js
@@ -0,0 +1,11 @@
+const name = "Parwej";
+cont age = "23";
+
+function isAbove18(age)
+{
+    return age >=18;
+}
+function add(a, b)
+{
+    return a + b;
+}
